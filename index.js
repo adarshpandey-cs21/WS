176d2f3c2b76539f1f2b2233193b1e46b63fef48
Adarsh Kumar Pandey <fakp24@gmail.com>
2023-09-08 17:38:04 +0530

    static files setup

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const port=8000;
 const expressLayouts=require('express-ejs-layouts');
 
 app.use(expressLayouts); //using express layouts
+app.use(express.static('./assets')) //setting static file for layout
+
+
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
 
 //using router
 app.use('/',require('./routes/index'))
