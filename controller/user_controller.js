fe5e69a522589d0607307bbfd39cccc8dea37192
Adarsh Kumar Pandey <fakp24@gmail.com>
2023-09-08 16:49:46 +0530

    view

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,3 +1,7 @@
 module.exports.profile=function(req,res){
-    return res.end('<h1>user profile click to follow lol</h1>')
+    // return res.end('<h1>user profile click to follow lol</h1>')
+
+    return res.render('usersProfile',{
+        title:"usersProfile!"
+    })
 }
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express=require('express');
 const app=express();
-const port=8000;
+const port=8000; 
+const expressLayouts=require('express-ejs-layouts');
+
+app.use(expressLayouts); //using express layouts
 
 //using router
 app.use('/',require('./routes/index'))
+
+//using view enjine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err){
