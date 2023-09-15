58e011df4ee6606eab0b82651d10008fcda57c78
Adarsh Kumar Pandey <fakp24@gmail.com>
2023-09-15 22:17:19 +0530

    post delete function

--- controller/posts_controller.js
+++ controller/posts_controller.js
@@ -14,23 +14,31 @@ module.exports.create=function(req,res){
 }
 
 
-module.exports.destroy=function(req,res){
-    Post.findOne({ _id: req.params.id })
-        .then((post)=>{
-            //.id means converting object id into string
-            if(post.user==req.user.id){
-                post.remove();
+module.exports.destroy = async function (req, res) {
+    try {
+        // Find the post by its ID
+        const post = await Post.findOne({ _id: req.params.id });
 
-                Comment.deleteMany({post:req.params.id})
-                        .then(()=>{
-                            return res.redirect('back');
-                        })
+        if (!post) {
+            // Handle the case where no post with the given ID is found
+            return res.redirect('back');
+        }
 
-            }{
-                return res.redirect('back');
-            }
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-}
+        // Check if the user owns the post (compare as strings)
+        if (post.user.toString() === req.user.id.toString()) {
+            // Remove the post
+            await Post.deleteOne({ _id: post._id });
+
+            // Remove associated comments
+            await Comment.deleteMany({ post: req.params.id });
+
+            return res.redirect('back');
+        } else {
+            // If the user doesn't own the post, redirect back
+            return res.redirect('back');
+        }
+    } catch (err) {
+        console.error(err);
+        return res.redirect('back');
+    }
+};
